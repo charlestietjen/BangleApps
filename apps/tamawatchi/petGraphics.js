6e33ab46c1b348606a290e46445127f65513939d
Charles Tietjen <tietjen.charles@gmail.com>
2024-12-17 19:29:28 -0500

    tamawatchi/graphics import from external js

---
+++ apps/tamawatchi/petGraphics.js
@@ -0,0 +1,3 @@
+const imgPetStage_1 = require("heatshrink").decompress(atob("iEQ4kA///m3bg8pI3kikQGEgURiMgAwchA4MSA4QGBkMSCAYOCDAgHEDAQHBiQCCFwgwFHwJAGACgA=="));
+
+module.exports = { imgPetStage_1}
